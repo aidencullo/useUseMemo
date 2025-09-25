d1fdc52b1b7a3aac11a622bd5af12d4efcdcbdf2
Aiden Cullo <aidencullo@gmail.com>
2025-09-25 11:33:26 -0400

    import useMemo

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import { useMemo } from 'react'
+
+function App() {
+    console.log("rendering <App/>")
+    
+    // const result = useMemo(() => console.log())
+    const result = 1
+
+    return result
+}
 
 export default App
